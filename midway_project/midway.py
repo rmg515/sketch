7dc5c939ebc935299a11fdabc255ff5e2ff421b9
Rheanna George <rmg515@gmail.com>
2018-02-21 19:50:57 -0500

    initial commit

---
+++ midway_project/midway.py
@@ -0,0 +1,24 @@
+from sys import argv 
+
+script, filename = argv
+
+txt = open(filename)
+target = open(filename, 'r') 
+
+
+state = raw_input("Please enter a state.")
+
+target.readline()
+
+# if (
+# FOR-LOOP EVIDENTLY NEEDED 
+
+
+# print "Let's read CSV file %r:" % filename
+
+
+# import argparse
+# parser = argparse.ArgumentParser()
+# parser.parse_args()
+
+ 
