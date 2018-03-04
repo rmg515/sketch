8352432eb85f38108d8fb6606edf1cb3c187ac6f
Rheanna George <rmg515@gmail.com>
2018-03-03 21:24:21 -0500

    deleted dead stuff

--- midway_project/midway.py
+++ midway_project/midway.py
@@ -115,41 +115,6 @@ def compass_searcher():
       print "What?" 
       break
   
-
-#for x in target: 
-#  if    
-
-   
-
-
-
-
-
-
-#from sys import argv 
-
-#script, filename = argv
-
-#txt = open(filename)
-#target = open(filename, 'r') 
-
-
-#state = raw_input("Please enter a state.")
-
-#target.readline()
-
-# if (
-# FOR-LOOP EVIDENTLY NEEDED 
-
-
-# print "Let's read CSV file %r:" % filename
-
-
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.parse_args()
-
-
 if user_input.lower() == "s":
   state_searcher() 
 if user_input.lower() == "r":
